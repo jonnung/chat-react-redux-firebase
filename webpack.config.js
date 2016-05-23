9f6265161c0b881d21c70ce537807132646ba9fc
EunwooCho <jonnung@gmail.com>
2016-05-24 02:58:53 +0900

    index.html 에서 inline 으로 삽입한 style, script 태그를 webpack 을 이용해 번들 파일을 만들고 자동으로 index.html에 추가 되도록 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
+
 module.exports = {
     entry: {
-        app: './src/app.js',
-        example: './src/example.js'
+        main: './src/main.js',
+        vendor: [
+            'react',
+            'react-dom'
+        ]
     },
     output: {
         path: __dirname + '/dist',
-        publicPath: "/assets/",
+        publicPath: "/",
         filename: '[name].bundle.js'
+    },
+    resolve: {
+        modulesDirectories: ['node_modules', 'bower_components', 'bower_components/bootstrap/dist/css']
     },
     devServer: {
         inline: true,
@@ -15,6 +25,12 @@ module.exports = {
     module: {
         loaders: [
             {
+                // entry 에서 지정한 이름별로 해당 js 에서 require 하는 css 파일을 불러옴
+                // require('./styles/main.css); /* in main.js */ '
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
                 test: /\.js$/,
                 loader: 'babel',
                 exclude: /node_modules/,
@@ -22,8 +38,35 @@ module.exports = {
                     cacheDirectory: true,
                     presets: ['react']
                 }
+            },
+            {
+                test: /\.woff$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"
+            },
+            {
+                test: /\.woff2$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"
+            },
+            {
+                test: /\.(eot|ttf|svg|gif|png)$/,
+                loader: "file-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            inject: 'body'
+        }),
+        new ExtractTextPlugin("[name].css"),
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+        ),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
 };
 
