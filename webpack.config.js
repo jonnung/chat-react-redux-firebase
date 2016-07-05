e1ef4b33869de802866c80be6d213eb2df8462e4
EunwooCho <jonnung@gmail.com>
2016-07-06 03:03:21 +0900

    Webpack config 파일 수정: font-awesome 안에 포함된 파일명(woff, eot 등)에 버전(ex. v.0.0.1)이 포함될때 정상적으로 파싱하지 못하는 문제 해결

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         filename: 'static/js/[name].bundle.js'
     },
     resolve: {
-        modulesDirectories: ['node_modules', 'src/bower_components', 'src/component']
+        modulesDirectories: ['node_modules', 'src/bower_components']
     },
     devServer: {
         inline: true,
@@ -27,28 +27,28 @@ module.exports = {
             {
                 // entry 에서 지정한 이름별로 해당 js 에서 require 하는 css 파일을 불러옴
                 // require('./styles/main.css); /* in main.js */ '
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+                test: /\.[s]?css$/,
+                loaders: ['style', 'css', 'sass']
             },
             {
-                test: /\.jsx$/,
+                test: /\.js[x]?$/,
                 loader: 'babel',
                 exclude: /node_modules/,
                 query: {
                     cacheDirectory: true,
-                    presets: ['react']
+                    presets: ['es2015', 'react']
                 }
             },
             {
-                test: /\.woff$/,
+                test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "url-loader?limit=10000&mimetype=application/font-woff&name=static/fonts/[name].[ext]"
             },
             {
-                test: /\.woff2$/,
+                test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=static/fonts/[name].[ext]"
             },
             {
-                test: /\.(eot|ttf|svg|gif|png)$/,
+                test: /\.(eot|ttf|svg|gif|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "file-loader?name=static/fonts/[name].[ext]"
             }
         ]
@@ -61,6 +61,8 @@ module.exports = {
         }),
         new ExtractTextPlugin("static/css/[name].css"),
         new webpack.ResolverPlugin(
+            // 위 resolve 에서 정의한 modulesDirectories 에서 \
+            // require() 또는 import 한 패키지를 찾기 위해서 bower.json 의 main 속성을 참조함
             new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
         ),
         new webpack.ProvidePlugin({
