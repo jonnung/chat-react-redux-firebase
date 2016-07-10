003b66808dd21bafc32f0d6e630028b86e848490
EunwooCho <jonnung@gmail.com>
2016-07-10 17:46:43 +0900

    앱 배경 변경, 사용자 입력 폼 추가

---
+++ src/components/Welcome.js
@@ -0,0 +1,17 @@
+/**
+ * Welcome view component
+ */
+
+import React, {Component, PropTypes} from 'react';
+
+
+export default class Welcome extends Component {
+    render () {
+        return (
+            <div className="welcome">
+            </div>
+        );
+    };
+}
+
+Welcome.PropTypes = {};
